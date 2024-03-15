6c764d287f749494cdf2adeefc60289db07fb568
Atik0052 <161108509+Atik0052@users.noreply.github.com>
2024-03-15 12:33:09 +0530

    Next app upload
    Node modules to be install

---
+++ my-qr/pages/api/qr-generate.ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import qrcode from 'qrcode';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log('Received Request:', req.method);
+
+  try {
+    if (req.method === 'GET') {
+      const { phoneNumber, message } = req.query;
+      console.log('Received Data:', { phoneNumber, message });
+
+      const link = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+      const qrCodeDataURL = await qrcode.toDataURL(link);
+      return res.json({ link, qrCodeDataURL });
+    } 
+      
+    else if (req.method === 'POST') {
+      const { phoneNumber, message } = req.body;
+      console.log('Received Data:', { phoneNumber, message });
+
+      const link = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+      const qrCodeDataURL = await qrcode.toDataURL(link);
+      return res.json({ link, qrCodeDataURL });
+    } else {
+      // If the method is not GET or POST, return a 405 Method Not Allowed response
+      return res.status(405).json({ error: 'Method Not Allowed' });
+    }
+  } catch (error) {
+    console.error('Error in generateQRCode:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
+
